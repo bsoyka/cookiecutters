96028562f2c79f819eeeb73a6c031e533cea8107
Ben Soyka <hello@bsoyka.me>
2025-07-29 15:28:30 -0600

    feat(python-library): add testing/coverage workflow

--- python-library/hooks/post_gen_project.py
+++ python-library/hooks/post_gen_project.py
@@ -1,3 +1,4 @@
 if __name__ == "__main__":
     print("Next steps:")
     print("- Set up GitHub Actions as a Trusted Publisher on PyPI: https://pypi.org/manage/account/publishing/")
+    print("- Set up the project on Codecov and add the token as the CODECOV_TOKEN secret on GitHub.")
