a62ab2712a7ce1a0f61fa6fa09bdc55d58a0a26c
Ben Soyka <hello@bsoyka.me>
2025-08-02 12:13:31 -0600

    chore(python-library): update post-gen steps

--- python-library/hooks/post_gen_project.py
+++ python-library/hooks/post_gen_project.py
@@ -1,4 +1,5 @@
 if __name__ == "__main__":
     print("Next steps:")
     print("- Set up GitHub Actions as a Trusted Publisher on PyPI: https://pypi.org/manage/account/publishing/")
-    print("- Set up the project on Codecov and add the token as the CODECOV_TOKEN secret on GitHub.")
+    print("- Set up the project on Codecov and add the token as the CODECOV_TOKEN secret on GitHub: https://app.codecov.io/")
+    print("- Publish documentation to Read the Docs and add the link around the project: https://app.readthedocs.org/dashboard/")
